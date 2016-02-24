30ff34b70d757c362e2182065ff1643d805f9685
Jason Gilliland <jason.gilliland@merck.com>
2016-02-24 14:52:50 -0500

    Fix constant max sequence length
    Prevent crashing and other problems by lengthening maximum sequence length.
    This change due to Jason Hughes.

--- utils.h
+++ utils.h
@@ -9,7 +9,7 @@
 
 #define MAX_ID_LEN (256)
 #define MAX_FN_LEN (512)
-#define MAX_SEQ_LEN (256)
+#define MAX_SEQ_LEN (1024)
 //60+33 = 93 = '[' (was 83='S')
 #define MAX_QUAL (93)
 extern char maximum_quality;
